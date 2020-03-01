668439559ac324dcad4d55fe08ad2dd4af17bcc4
Volha <shaukavets@gmail.com>
2020-03-01 17:17:14 +0300

    add test func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ((Array.isArray(array))&&(array.length>0)){
+    array.sort((a, b)=> a - b);    
+    return (array[0])}
+  else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ((Array.isArray(array))&&(array.length>0)){
+    array.sort((a, b)=> a - b);    
+    return array[array.length-1]}
+  else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((Array.isArray(array))&&(array.length>0)){
+    let sum=array.reduce((x,y)=>x+y) 
+    return (sum/array.length)}
+  else return 0;
+
 }
